3fefd02542f0bb8887487e52b91537b6cae6f96e
dEfy310 <musinov_2004@mail.ru>
2022-02-12 14:41:15 +0300

    Initial commit

---
+++ src/main/java/pro/sky/java/course2/lesson6map/Lesson6MapApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.java.course2.lesson6map;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lesson6MapApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lesson6MapApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pro/sky/java/course2/lesson6map/Lesson6MapApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.java.course2.lesson6map;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lesson6MapApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
